f1c0fd5786c806dd5ad0e901dd271f3215d9d408
sanjeevpratapsingh <sanjeevpratapsingh93@gmail.com>
2017-03-02 19:20:07 -0800

    Some Changes on Functions.php and Added page.php

--- functions.php
+++ functions.php
@@ -8,32 +8,24 @@ function includingStyleSheet(){
 
 add_action('wp_enqueue_scripts','includingStyleSheet');
 
-// Navigation Menus
-
-function navigation_menu(){
-register_nav_menus(array(
-    'primary' => __('Primary Menu'),
-    'footer' =>__('Secondary Menu'),
-));
-}
-add_action( 'init', 'navigation_menu' );
-
-
 //Custom Excerpt Link
-
 function custom_excerpt_length(){
     return 30;
 }
 
 add_filter('excerpt_length','custom_excerpt_length');
 
+
 //Setting Up Some WordPress Resources
 
-
-function addingSupport(){
-//Add Featured Image Support
-add_theme_support( 'post-thumbnails' ); 
+function wordPress_resources(){
+//Navigation Menu Support
+register_nav_menus(array(
+    'primary' => __('Primary Menu'),
+    'footer' =>__('Secondary Menu'),
+));
     
+//Featured Image Support    
+add_theme_support('post-thumbnails'); 
 }
-
-add_action('init','addingSupport');
+add_action('after_setup_theme', 'wordPress_resources' );
